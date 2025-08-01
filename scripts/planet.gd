5868f4457c32b5334f6c570d9b4581e8b0bc752c
Alisa Pivapysko <pvpsko@ik.me>
2025-08-01 21:29:46 +0200

    fix movement

--- scripts/planet.gd
+++ scripts/planet.gd
@@ -1,11 +1,6 @@
 extends Sprite2D
 
-const day_length := 180 # seconds
-const rotation_speed := 0.5
+const day_length := 60 # seconds
 
 func _process(delta: float) -> void:
 	rotation -= deg_to_rad(360 / day_length * delta)
-	 #if Input.is_action_pressed("ui_left"):
-	 	#rotation += 1 * delta
-	 #if Input.is_action_pressed("ui_right"):
-	 	#rotation -= 1 * delta
--- scripts/prince.gd
+++ scripts/prince.gd
@@ -1,39 +1,42 @@
 extends Sprite2D
 
-const walk_speed := 0.5
-const position_limit = 1.3
+const walk_speed := 0.5 # rads/s
+const position_limit = 1.3 # +-rads
 
 var start_pos: Vector2
-var target_rotation := -0.3
-var is_wątering_can_full := false # looks like the begining of some great shitcode
+var target_reached := true
+var target_rotation := 0.0
+#var is_wątering_can_full := false
 
-func _init() -> void:
+func _ready() -> void:
 	start_pos = position
 
 func _process(delta: float) -> void:
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+	if Input.is_action_just_pressed("use"):
 		var mouse_pos: Vector2 = %Planet.get_local_mouse_position()
 		target_rotation = -mouse_pos.angle_to(Vector2.UP)
-		print(rad_to_deg(target_rotation))
+		target_reached = false
 	update_position(delta)
-	if Input.is_action_just_released("use"):
-		if position.distance_to(%Well.position) < 50 and !is_wątering_can_full:
-			is_wątering_can_full = true
-			print("watering can filled")
-		if position.distance_to(%Rose.position) < 50 and is_wątering_can_full:
-			is_wątering_can_full = false
-			print("rose watered")
+	#if Input.is_action_just_released("use"):
+		#if position.distance_to(%Well.position) < 50 and !is_wątering_can_full:
+			#is_wątering_can_full = true
+			#print("watering can filled")
+		#if position.distance_to(%Rose.position) < 50 and is_wątering_can_full:
+			#is_wątering_can_full = false
+			#print("rose watered")
 
 func update_position(delta: float) -> void:
-	if rotation > target_rotation:
-		rotation -= walk_speed * delta
-		rotation = target_rotation if rotation < target_rotation else rotation
-		flip_h = true
-	elif rotation < target_rotation:
-		rotation += walk_speed * delta
-		rotation = target_rotation if rotation > target_rotation else rotation
-		flip_h = false
-  
+	var normalized_diff = wrapf(rotation - target_rotation, -PI, PI)
+	if not target_reached:
+		if normalized_diff > 0:
+			rotation -= walk_speed * delta
+			flip_h = true
+		elif normalized_diff < 0:
+			rotation += walk_speed * delta
+			flip_h = false
+		if abs(wrapf(rotation - target_rotation, -PI, PI)) < walk_speed * delta:
+			print("target_reached")
+			target_reached = true
 	rotation = clamp(
 		rotation + %Planet.rotation, -position_limit, position_limit
 	) - %Planet.rotation
