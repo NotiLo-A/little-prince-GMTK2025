9c712fc47400f39b1fbb3d90ea4697809cd0f52a
MiHaniKo <139514821+NotiLo-A@users.noreply.github.com>
2025-08-01 16:46:50 +0300

    add sky

--- scripts/planet.gd
+++ scripts/planet.gd
@@ -5,7 +5,7 @@ const rotation_speed := 0.5
 
 func _process(delta: float) -> void:
 	rotation -= deg_to_rad(360 / day_length * delta)
-	# if Input.is_action_pressed("ui_left"):
-	# 	rotation += 1 * delta
-	# if Input.is_action_pressed("ui_right"):
-	# 	rotation -= 1 * delta
+	 #if Input.is_action_pressed("ui_left"):
+	 	#rotation += 1 * delta
+	 #if Input.is_action_pressed("ui_right"):
+	 	#rotation -= 1 * delta
