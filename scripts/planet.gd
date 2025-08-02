8c125ceb1ee4738d6ef6395ba010739c33d726de
rapuska <mikalajsiatkouski@gmail.com>
2025-08-02 18:24:30 +0300

    Export planet speed

--- scripts/planet.gd
+++ scripts/planet.gd
@@ -1,9 +1,11 @@
 extends Sprite2D
 
-@export var day_length := 100 # seconds
-var rotation_speed := 2 * PI / day_length # rad/s
+@export var day_length := 100.0 # seconds
+
+var rotation_speed: float
 
 func _ready() -> void:
+	rotation_speed = 2 * PI / day_length # rad/s
 	for node in get_tree().get_nodes_in_group("interactive"):
 		node.add_child(preload("res://scenes/glow_by_mouse.tscn").instantiate())
 
