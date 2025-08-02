d530ec427ac2d3da01c3ba9b2f419f2fa8420f29
Alisa Pivapysko <pvpsko@ik.me>
2025-08-02 16:19:29 +0200

    fix oopsie woopsie

--- scripts/planet.gd
+++ scripts/planet.gd
@@ -5,7 +5,7 @@ const rotation_speed = 2 * PI / day_length # rad/s
 
 func _ready() -> void:
 	for node in get_tree().get_nodes_in_group("interactive"):
-		node.add_child(preload("res://scenes/glow_by_mouse.tscn").instantiate())
+		node.add_child(preload("res://scenes/outline_by_mouse.tscn").instantiate())
 
 func _process(delta: float) -> void:
 	rotation -= rotation_speed * delta
