b165fe797afbea9bf8ed1bf105f44a88ec9b3f22
rapuska <mikalajsiatkouski@gmail.com>
2025-08-03 16:36:31 +0300

    display carried item

---
+++ scripts/util/carried_items_view.gd
@@ -0,0 +1,26 @@
+extends MarginContainer
+
+@export var tf: int
+@export var item_icon: TextureRect
+@export var watering_can_texture: Texture
+
+func _ready() -> void:
+	item_icon.visible = false
+
+func _process(_delta: float) -> void:
+	var prince: Prince = GlobalNodes.prince
+	if prince == null:
+		item_icon.visible = false
+		return
+
+	if prince.carried_item == null:
+		item_icon.visible = false
+		return
+
+	var is_watering_can = prince.carried_item.name.contains("WateringCan")
+	if is_watering_can:
+		item_icon.visible = true
+		item_icon.texture = watering_can_texture
+		return
+
+	item_icon.visible = false
