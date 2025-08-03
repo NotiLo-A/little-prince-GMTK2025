f11d1c6ddea79485811992273512239955859f80
rapuska <mikalajsiatkouski@gmail.com>
2025-08-03 19:41:16 +0300

    baobabs slow down

--- scripts/prince.gd
+++ scripts/prince.gd
@@ -1,4 +1,5 @@
-class_name Prince extends AnimatedSprite2D
+class_name Prince
+extends AnimatedSprite2D
 
 @export var walk_speed := 1.5 # rads/s
 @export var position_limit = 1.3 # +-rads
@@ -10,6 +11,8 @@ var target_rotation := 0.0
 var target_object: Node2D
 var was_target_assigned_this_frame := false
 
+var speed_multiplier := 1.0
+
 func _ready() -> void:
 	GlobalNodes.prince = self
 	start_pos = position
@@ -20,7 +23,6 @@ func _process(delta: float) -> void:
 		call_prince_to_place(-mouse_pos.angle_to(Vector2.UP))
 	move(delta)
 	was_target_assigned_this_frame = false
-
 
 func call_prince_to_interact(where: Node2D):
 	target_reached = false
@@ -38,15 +40,17 @@ func call_prince_to_place(where: float):
 func move(delta: float) -> void:
 	play("idle" if target_reached else "walk")
 	var normalized_diff = wrapf(rotation - target_rotation, -PI, PI)
+	var scaled_speed = walk_speed * speed_multiplier * delta
+
 	if !target_reached:
 		if normalized_diff > 0:
-			rotation -= walk_speed * delta
+			rotation -= scaled_speed
 			flip_h = true
 		elif normalized_diff < 0:
-			rotation += walk_speed * delta
+			rotation += scaled_speed
 			flip_h = false
-		if abs(wrapf(rotation - target_rotation, -PI, PI)) < walk_speed * delta:
-			print("target reached")
+
+		if abs(wrapf(rotation - target_rotation, -PI, PI)) < scaled_speed:
 			target_reached = true
 			if target_object != null:
 				target_object.interact(carried_item)
@@ -56,3 +60,11 @@ func move(delta: float) -> void:
 	) - GlobalNodes.planet.rotation
 	# wierd ass correction because of the long scarf at the side of the sprite
 	position = start_pos.rotated(rotation + (.05 if flip_h else -.05))
+
+func slow_down(mult: float):
+	print("slow")
+	print(walk_speed * 0.16 * speed_multiplier)
+	speed_multiplier = mult
+
+func reset_speed():
+	speed_multiplier = 1.0
