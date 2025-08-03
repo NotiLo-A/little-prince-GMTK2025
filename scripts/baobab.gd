d260af0b7f9c8ee7766975640f7f7d01641945ff
Alisa Pivapysko <pvpsko@ik.me>
2025-08-03 19:35:20 +0200

    fix baobabs

--- scripts/baobab.gd
+++ scripts/baobab.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 enum States { None, Sprout, GrownTree }
 
-@export var sprout_sprite: Sprite2D
-@export var tree_sprite: Sprite2D
+@onready var sprout_sprite := $Sprout
+@onready var tree_sprite := $Tree
 
 @export var slow_range := 100
 @export var slow_rate := 0.5
@@ -11,6 +11,13 @@ enum States { None, Sprout, GrownTree }
 
 var state := States.None
 var prince_was_in_range: bool
+
+func interact(carried_item: Node2D) -> void:
+	if carried_item != Hoe:
+		return
+	if state == States.Sprout:
+		state = States.None
+	update_state()
 
 func _ready() -> void:
 	update_state()
@@ -21,7 +28,6 @@ func _process(_delta: float) -> void:
 		return
 
 	var prince = GlobalNodes.prince as Prince
-
 	if prince == null:
 		return
 
@@ -29,17 +35,16 @@ func _process(_delta: float) -> void:
 	if distance < slow_range:
 		if prince_was_in_range:
 			return
-
 		prince_was_in_range = true
 		prince.slow_down(slow_rate)
 	else:
 		if !prince_was_in_range:
 			return
-
 		prince_was_in_range = false
 		prince.reset_speed()
 
 func update_state():
+	$Area2D.input_pickable = state != States.None
 	if state == States.Sprout:
 		sprout_sprite.visible = true
 		tree_sprite.visible = false
@@ -62,14 +67,12 @@ func _on_high() -> void:
 	pass # Replace with function body.
 
 func _on_low() -> void:
-	var proc = randf() > change_to_grow
+	var proc = randf() < change_to_grow
 	if proc:
 		grow()
 
 func grow():
 	state += 1
-
 	if state > States.GrownTree:
 		state = States.GrownTree
-
 	update_state()
