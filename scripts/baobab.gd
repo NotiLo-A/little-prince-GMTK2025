038e7777208a7f365d3fff2ce5da443b4bb9347e
rapuska <mikalajsiatkouski@gmail.com>
2025-08-03 19:06:38 +0300

    baobabs grow

--- scripts/baobab.gd
+++ scripts/baobab.gd
@@ -9,6 +9,7 @@ var state := States.None
 
 func _ready() -> void:
 	update_state()
+	randomize()
 
 func update_state():
 	if state == States.Sprout:
@@ -20,4 +21,20 @@ func update_state():
 	else:
 		sprout_sprite.visible = false
 		tree_sprite.visible =  false
-		
+
+func _on_high() -> void:
+	pass # Replace with function body.
+
+func _on_low() -> void:
+	var proc = randf() > 0.8
+	print(proc)
+	if proc:
+		grow()
+
+func grow():
+	state += 1
+
+	if state > States.GrownTree:
+		state = States.GrownTree
+
+	update_state()
